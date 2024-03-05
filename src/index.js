94b5cf4f0d6d61b0c71dbfb31537c0354ef50867
Edvard <edvardastapovich@gmail.com>
2024-03-05 22:09:22 +0100

    feat: add servise file, test recieving api from server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
+import MarvelService from './service/MarvelService';
 import './style/style.scss';
+
+const marvelService = new MarvelService();
+
+marvelService.getAllCharacters().then(res => console.log(res.data.results))
+marvelService.getCharacter(1011052).then(res => console.log(res.data.results))
 
 ReactDOM.render(
   <React.StrictMode>
